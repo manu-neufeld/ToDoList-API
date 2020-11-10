91bae52b87d2387e4566efb2d08c740d8dae206f
manu-neufeld <manuela.neufeld@gmail.com>
2020-11-10 12:10:37 +0000

    begin tables

--- src/models.py
+++ src/models.py
@@ -1,12 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeingKey, Integer, String
+
 
 db = SQLAlchemy()
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_to_do = db.Column(db.String(80),db.ForeingKey("User.user_name"))
+    label = db.Column(db.String(120), unique=False, nullable=False)
+    done = db.Column(db.Boolean(False), unique=False, nullable=False)
+    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_name = db.Column(db.String(80), unique=True, nullable=False)
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
